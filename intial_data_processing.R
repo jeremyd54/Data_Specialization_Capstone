9d0b8cc184800ad88702c9667240d77d792cea07
Jeremy Dean <sdean.jeremy@gmail.com>
2021-02-18 16:09:07 -0800

    Made english text collection

--- intial_data_processing.R
+++ intial_data_processing.R
@@ -1,3 +1,13 @@
+library(tm)
+
+## Download and unzip data
 dataUrl <- "https://d396qusza40orc.cloudfront.net/dsscapstone/dataset/Coursera-SwiftKey.zip"
 download.file(dataUrl, destfile = "data.zip")
 unzip("data.zip")
+
+
+## Make text collections
+eng <- Corpus(DirSource("./final/en_US"),
+              readerControl = list(reader = readPlain, language = "en_US",
+                                   load = TRUE))
+            
