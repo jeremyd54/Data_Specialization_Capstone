aeda4d612d9f73f72e41f90ba88bf88f279e5ab8
Jeremy Dean <sdean.jeremy@gmail.com>
2021-03-16 16:36:16 -0700

    Common words found

--- exploratory_analysis.R
+++ exploratory_analysis.R
@@ -1,3 +1,40 @@
 source("intial_data_processing.R")
 
+## Import stop words
+data("stop_words")
+
+### Find common words
+
+## twitter
+twitCommon <- twit %>%
+    count(word, sort = TRUE)
+head(twitCommon, n = 100)
+#Remove stop words
+twitCommonStop <- twit %>%
+    anti_join(stop_words) %>%
+    count(word, sort = TRUE)
+head(twitCommonStop, n = 10)
+
+## Blogs
+blogCommon <- blog %>%
+    count(word, sort = TRUE)
+head(blogCommon, n = 10)
+#Remove stop words
+blogCommonStop <- blog %>%
+    anti_join(stop_words) %>%
+    count(word, sort = TRUE)
+head(blogCommonStop, n = 20)
+
+## News
+newsCommon <- news %>%
+    count(word, sort = TRUE)
+head(newsCommon, n = 10)
+#Remove stop words
+newsCommonStop <- news %>%
+    anti_join(stop_words) %>%
+    count(word, sort = TRUE)
+head(newsCommonStop, n = 10)
+
+
+
 
