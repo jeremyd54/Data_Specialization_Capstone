b447a04ddf6895ae9841d65bff9138eafe866be9
Jeremy Dean <sdean.jeremy@gmail.com>
2021-03-15 19:13:58 -0700

    Added packages, read in data and convert sources into a long form

--- intial_data_processing.R
+++ intial_data_processing.R
@@ -1,9 +1,28 @@
 library(tm)
+library(dplyr)
+library(tidyr)
+library(tidytext)
 
 ## Download and unzip data
 dataUrl <- "https://d396qusza40orc.cloudfront.net/dsscapstone/dataset/Coursera-SwiftKey.zip"
 download.file(dataUrl, destfile = "data.zip")
 unzip("data.zip")
 
+#Read in data and transform into long format
 
-            
+twitLines <- readLines("./final/en_US/en_US.twitter.txt")
+twit <- tibble(entry = 1:length(twitLines), text = twitLines) %>%
+    unnest_tokens(word, text)
+
+blogLines <- readLines("./final/en_US/en_US.blogs.txt")
+blog <- tibble(entry = 1:length(blogLines), text = blogLines) %>%
+    unnest_tokens(word, text)
+
+newsLines <- readLines("./final/en_US/en_US.news.txt")
+news <- tibble(entry = 1:length(newsLines), text = newsLines) %>%
+    unnest_tokens(word, text)
+
+
+
+
+
