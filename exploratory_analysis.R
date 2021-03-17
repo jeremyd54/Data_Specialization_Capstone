1cff8ac7645309a79027fc6890b0fbbd5fdfe9c7
Jeremy Dean <sdean.jeremy@gmail.com>
2021-03-16 19:26:42 -0700

    Added common twitter words plots, changed how many words display

--- exploratory_analysis.R
+++ exploratory_analysis.R
@@ -1,4 +1,7 @@
 source("intial_data_processing.R")
+library(ggplot2)
+library(gtable)
+library(gridExtra)
 
 ## Import stop words
 data("stop_words")
@@ -8,17 +11,17 @@ data("stop_words")
 ## twitter
 twitCommon <- twit %>%
     count(word, sort = TRUE)
-head(twitCommon, n = 100)
+head(twitCommon, n = 20)
 #Remove stop words
 twitCommonStop <- twit %>%
     anti_join(stop_words) %>%
     count(word, sort = TRUE)
-head(twitCommonStop, n = 10)
+head(twitCommonStop, n = 20)
 
 ## Blogs
 blogCommon <- blog %>%
     count(word, sort = TRUE)
-head(blogCommon, n = 10)
+head(blogCommon, n = 20)
 #Remove stop words
 blogCommonStop <- blog %>%
     anti_join(stop_words) %>%
@@ -28,12 +31,29 @@ head(blogCommonStop, n = 20)
 ## News
 newsCommon <- news %>%
     count(word, sort = TRUE)
-head(newsCommon, n = 10)
+head(newsCommon, n = 20)
 #Remove stop words
 newsCommonStop <- news %>%
     anti_join(stop_words) %>%
     count(word, sort = TRUE)
-head(newsCommonStop, n = 10)
+head(newsCommonStop, n = 20)
+
+## Plot common words
+#Twitter
+twitComP1 <- twitCommon %>% 
+    filter(n > 168000) %>%
+    mutate(word = reorder(word, n)) %>%
+    ggplot(aes(n, word)) + ggtitle("With Stop Words") +
+    geom_col(fill = "blue")   
+twitComP2 <- twitCommonStop %>%
+    filter(n > 28450) %>%
+    mutate(word = reorder(word, n)) %>%
+    ggplot(aes(n, word)) + ggtitle("Stop Words Removed") +
+    geom_col(fill = "red")
+grid.arrange(twitComP1,twitComP2, nrow = 1, 
+             top = "Most Common Twitter Words")
+
+
 
 
 
